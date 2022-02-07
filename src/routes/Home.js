4d126898210886f429e03b36c2bbd0c186da66f4
jungwonJung <wjdwjd1501@gmail.com>
2022-02-08 00:59:23 +0900

    2022.02.08

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,69 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react/cjs/react.development";
+import { dbService } from "../fbase";
+import Nweet from "../components/Nweet";
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  // const getNweets = async () => {
+  //   const dbNweets = await dbService.collection("nweets").get();
+  //   dbNweets.forEach((document) => {
+  //     const nweetObject = {
+  //       ...document.data(),
+  //       id: document.id,
+  //     };
+  //     setNweets((prev) => [nweetObject, ...prev]);
+  //   });
+  // };
+  useEffect(() => {
+    // getNweets();
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      createrId: userObj.uid,
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on ur mind?"
+          maxLength={120}
+        />
+        <input type="submit" placeholder="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.createrId === userObj.uid}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
